cf486612f46b7fa055b4d2ae5bf7eb13722386dd
Goddy-2024 <gaoajuoga@gmail.com>
2025-08-07 03:02:22 +0300

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 export default async function connectDB(){
       try{
-            const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/fellowship_db';
+            const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://gaoajuoga:tq7CN60C5VaTHNIB@cluster0.axn0rap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
             await mongoose.connect(mongoUri);
-            console.log(`MongoDB integration success at: ${mongoUri}`);
+            console.log(`MongoDB integration success !!!`);
             console.log(`connected to client: ${process.env.CLIENT_URL || 'http://localhost:5174'}`);
       }catch(error){
             console.error("Failed to connect to MONGODB:", error.message);
